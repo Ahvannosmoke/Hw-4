c9f275c4955a8464a58f82e9f53ffa07cfaba846
Sovann Sam <samsovann2003@gmail.com>
2024-03-06 08:12:30 -0800

    first push

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,13 @@
+# CMake generated Testfile for 
+# Source directory: /home/vann/catkin_ws/src
+# Build directory: /home/vann/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("teleop_twist_keyboard")
+subdirs("smb_common/smb_control")
+subdirs("smb_common/smb_gazebo")
+subdirs("vann_test")
+subdirs("vann_tutorials")
+subdirs("smb_common/smb_description")
---
+++ build/vann_tutorials/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/vann/catkin_ws/src/vann_tutorials
+# Build directory: /home/vann/catkin_ws/build/vann_tutorials
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
